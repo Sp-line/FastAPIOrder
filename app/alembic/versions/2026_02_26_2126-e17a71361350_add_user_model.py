ced7d3716441714d03d7f3a997256551fbc9e303
spline <dmcemin@gmail.com>
2026-02-26 21:30:37 +0100

    add: User model & migration

---
+++ app/alembic/versions/2026_02_26_2126-e17a71361350_add_user_model.py
@@ -0,0 +1,37 @@
+"""add User model
+
+Revision ID: e17a71361350
+Revises:
+Create Date: 2026-02-26 21:26:44.520002
+
+"""
+
+from typing import Sequence, Union
+
+from alembic import op
+import sqlalchemy as sa
+
+revision: str = "e17a71361350"
+down_revision: Union[str, None] = None
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    op.create_table(
+        "users",
+        sa.Column("id", sa.Integer(), nullable=False),
+        sa.Column("email", sa.String(length=255), nullable=False),
+        sa.Column("first_name", sa.String(length=100), nullable=False),
+        sa.Column("last_name", sa.String(length=100), nullable=False),
+        sa.Column("phone_number", sa.String(length=20), nullable=True),
+        sa.PrimaryKeyConstraint("id", name=op.f("pk_users")),
+        sa.UniqueConstraint("email", name=op.f("uq_users_email")),
+        sa.UniqueConstraint(
+            "phone_number", name=op.f("uq_users_phone_number")
+        ),
+    )
+
+
+def downgrade() -> None:
+    op.drop_table("users")
---
+++ app/core/models/user.py
@@ -0,0 +1,13 @@
+from sqlalchemy import String
+from sqlalchemy.orm import Mapped, mapped_column
+
+from constants import UserLimits
+from core.models import Base
+from core.models.mixins.int_id_pk import IntIdPkMixin
+
+
+class User(IntIdPkMixin, Base):
+    email: Mapped[str] = mapped_column(String(UserLimits.EMAIL_MAX), unique=True)
+    first_name: Mapped[str] = mapped_column(String(UserLimits.FIRST_NAME_MAX))
+    last_name: Mapped[str] = mapped_column(String(UserLimits.LAST_NAME_MAX))
+    phone_number: Mapped[str | None] = mapped_column(String(UserLimits.PHONE_MAX), unique=True)
