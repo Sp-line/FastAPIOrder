14ece8301fbe1c49771bb64a145c0c567e16099f
spline <dmcemin@gmail.com>
2026-02-28 22:20:04 +0100

    add: new user consts

--- app/constants/user.py
+++ app/constants/user.py
@@ -3,6 +3,10 @@ class UserLimits:
     EMAIL_UNIQUE: bool = True
 
     FIRST_NAME_MAX: int = 100
+    FIRST_NAME_MIN: int = 1
+
     LAST_NAME_MAX: int = 100
+    LAST_NAME_MIN: int = 1
 
     PHONE_MAX: int = 20
+    PHONE_MIN: int = 9
