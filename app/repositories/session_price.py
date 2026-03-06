9848f52fc398240154d4a74fe7f865a74444ea80
spline <dmcemin@gmail.com>
2026-03-07 00:50:46 +0100

    add: SessionPrice repo

---
+++ app/repositories/session_price.py
@@ -0,0 +1,44 @@
+from decimal import Decimal
+from typing import Sequence, Iterable
+
+from sqlalchemy import select, tuple_
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from constants import SeatType
+from core.models import SessionPrice
+from integrity_handlers.session_price import session_price_error_handler
+from repositories.base import RepositoryBase
+from schemas.session_price import SessionPriceCreateDB, SessionPriceUpdateDB, SessionPriceCombination
+
+
+class SessionPriceRepository(
+    RepositoryBase[
+        SessionPrice,
+        SessionPriceCreateDB,
+        SessionPriceUpdateDB,
+    ]
+):
+    def __init__(self, session: AsyncSession) -> None:
+        super().__init__(
+            model=SessionPrice,
+            session=session,
+            table_error_handler=session_price_error_handler,
+        )
+
+    async def get_prices_by_session_and_seat_types(
+            self,
+            combinations: Iterable[SessionPriceCombination]
+    ) -> Sequence[SessionPrice]:
+        tuple_filters = [(c.session_id, c.seat_type) for c in combinations]
+
+        if not tuple_filters:
+            return []
+
+        stmt = (
+            select(SessionPrice)
+            .where(
+                tuple_(SessionPrice.session_id, SessionPrice.seat_type).in_(tuple_filters)
+            )
+        )
+        result = await self._session.execute(stmt)
+        return result.scalars().all()
