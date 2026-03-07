386c9cf3d487a7aafed2b025da891a36d0472bdc
spline <dmcemin@gmail.com>
2026-03-07 22:19:30 +0100

    add: API for models

--- app/api/api_v1/__init__.py
+++ app/api/api_v1/__init__.py
@@ -1,7 +1,20 @@
 from fastapi import APIRouter
 
 from core.config import settings
+from .hall import router as hall_router
+from .session import router as session_router
+from .movie import router as movie_router
+from .user import router as user_router
+from .seat import router as seat_router
+from .session_price import router as session_price_router
 
 router = APIRouter(
     prefix=settings.api.v1.prefix,
 )
+
+router.include_router(hall_router, prefix="/halls", tags=["Halls"])
+router.include_router(session_router, prefix="/sessions", tags=["Sessions"])
+router.include_router(movie_router, prefix="/movies", tags=["Movies"])
+router.include_router(seat_router, prefix="/seats", tags=["Seats"])
+router.include_router(user_router, prefix="/users", tags=["Users"])
+router.include_router(session_price_router, prefix="/session-prices", tags=["Session-Prices"])
---
+++ app/api/api_v1/user.py
@@ -0,0 +1,37 @@
+from dishka.integrations.fastapi import DishkaRoute, FromDishka
+from fastapi import APIRouter
+
+from schemas.user import UserRead, UserCreateReq, UserUpdateReq
+from service.user import UserService
+
+router = APIRouter(route_class=DishkaRoute)
+
+
+@router.get("/")
+async def get_users(service: FromDishka[UserService], skip: int = 0, limit: int = 100) -> list[UserRead]:
+    return await service.get_all(skip, limit)
+
+
+@router.get("/{user_id}")
+async def get_user(user_id: int, service: FromDishka[UserService]) -> UserRead:
+    return await service.get_by_id(user_id)
+
+
+@router.post("/")
+async def create_user(data: UserCreateReq, service: FromDishka[UserService]) -> UserRead:
+    return await service.create(data)
+
+
+@router.post("/bulk")
+async def bulk_create_users(data: list[UserCreateReq], service: FromDishka[UserService]) -> list[UserRead]:
+    return await service.bulk_create(data)
+
+
+@router.patch("/{user_id}")
+async def update_user(user_id: int, data: UserUpdateReq, service: FromDishka[UserService]) -> UserRead:
+    return await service.update(user_id, data)
+
+
+@router.delete("/{user_id}")
+async def delete_user(user_id: int, service: FromDishka[UserService]) -> None:
+    return await service.delete(user_id)
