eab7142ef931d9bad8afecf672423a7808033c0d
spline <dmcemin@gmail.com>
2026-03-07 21:42:57 +0100

    add: UserService

---
+++ app/service/user.py
@@ -0,0 +1,25 @@
+from repositories.unit_of_work import UnitOfWork
+from repositories.user import UserRepository
+from schemas.user import UserRead, UserCreateReq, UserUpdateReq, UserCreateDB, UserUpdateDB
+from service.base import ServiceBase
+
+
+class UserService(
+    ServiceBase[
+        UserRepository,
+        UserRead,
+        UserCreateReq,
+        UserUpdateReq,
+        UserCreateDB,
+        UserUpdateDB,
+    ],
+):
+    def __init__(self, repository: UserRepository, unit_of_work: UnitOfWork) -> None:
+        super().__init__(
+            repository=repository,
+            unit_of_work=unit_of_work,
+            table_name="users",
+            read_schema=UserRead,
+            db_create_schema=UserCreateDB,
+            db_update_schema=UserUpdateDB,
+        )
