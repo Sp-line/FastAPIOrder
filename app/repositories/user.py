15bfca4b73b10390d84ff9db95c608a8960c101b
spline <dmcemin@gmail.com>
2026-03-07 21:35:12 +0100

    add: UserRepository

---
+++ app/repositories/user.py
@@ -0,0 +1,21 @@
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from core.models import User
+from integrity_handlers.user import user_error_handler
+from repositories.base import RepositoryBase
+from schemas.user import UserCreateDB, UserUpdateDB
+
+
+class UserRepository(
+    RepositoryBase[
+        User,
+        UserCreateDB,
+        UserUpdateDB,
+    ]
+):
+    def __init__(self, session: AsyncSession) -> None:
+        super().__init__(
+            model=User,
+            session=session,
+            table_error_handler=user_error_handler,
+        )
