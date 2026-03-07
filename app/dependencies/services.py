eaf00b0a84c0897fc8800357e549c012268284e4
spline <dmcemin@gmail.com>
2026-03-07 21:44:57 +0100

    add: UserService to ServiceProvider

--- app/dependencies/services.py
+++ app/dependencies/services.py
@@ -5,6 +5,7 @@ from service.movie import MovieService
 from service.seat import SeatService
 from service.session import SessionService
 from service.session_price import SessionPriceService
+from service.user import UserService
 
 
 class ServiceProvider(Provider):
@@ -15,3 +16,4 @@ class ServiceProvider(Provider):
     get_movie_service = provide(MovieService)
     get_seat_service = provide(SeatService)
     get_session_price_service = provide(SessionPriceService)
+    get_user_service = provide(UserService)
