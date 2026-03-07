b4588761ce6677a8e469cf7bb6a0dc6b1afad8b0
spline <dmcemin@gmail.com>
2026-03-07 21:27:02 +0100

    add: ServiceProvider

---
+++ app/dependencies/services.py
@@ -0,0 +1,17 @@
+from dishka import provide, Scope, Provider
+
+from service.hall import HallService
+from service.movie import MovieService
+from service.seat import SeatService
+from service.session import SessionService
+from service.session_price import SessionPriceService
+
+
+class ServiceProvider(Provider):
+    scope = Scope.REQUEST
+
+    get_hall_service = provide(HallService)
+    get_session_service = provide(SessionService)
+    get_movie_service = provide(MovieService)
+    get_seat_service = provide(SeatService)
+    get_session_price_service = provide(SessionPriceService)
