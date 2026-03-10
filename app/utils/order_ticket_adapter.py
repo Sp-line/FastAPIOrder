80b09d9a427287bdd79debc91c6c47b3074b71d4
spline <dmcemin@gmail.com>
2026-03-10 16:48:24 +0100

    add: OrderTicketAdapter

---
+++ app/utils/order_ticket_adapter.py
@@ -0,0 +1,15 @@
+from __future__ import annotations
+
+from typing import Any, TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from core.models import Order, Ticket
+
+
+class OrderTicketAdapter:
+    def __init__(self, order: Order, tickets: list[Ticket]):
+        self._order = order
+        self.tickets = tickets
+
+    def __getattr__(self, item: str) -> Any:
+        return getattr(self._order, item)
