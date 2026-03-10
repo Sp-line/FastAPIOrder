a113764f526f59ce4634d7a9e515a715c819efa2
spline <dmcemin@gmail.com>
2026-03-10 17:27:05 +0100

    add: Ticket query endpoints

---
+++ app/api/api_v1/ticket.py
@@ -0,0 +1,17 @@
+from dishka.integrations.fastapi import DishkaRoute, FromDishka
+from fastapi import APIRouter
+
+from schemas.ticket import TicketRead
+from service.ticket import TicketQueryService
+
+router = APIRouter(route_class=DishkaRoute)
+
+
+@router.get("/", summary="[Admin] Get Tickets")
+async def get_tickets(service: FromDishka[TicketQueryService], skip: int = 0, limit: int = 100) -> list[TicketRead]:
+    return await service.get_all(skip, limit)
+
+
+@router.get("/{ticket_id}", summary="[Admin] Get Ticket")
+async def get_ticket(ticket_id: int, service: FromDishka[TicketQueryService]) -> TicketRead:
+    return await service.get_by_id(ticket_id)
