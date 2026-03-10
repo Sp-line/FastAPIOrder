8933a7e7f91aed07e740a0c2bf7c4f64f2b40a3f
spline <dmcemin@gmail.com>
2026-03-10 17:08:48 +0100

    add: providers for TicketQueryRepository, TicketCommandRepository

--- app/dependencies/repositories.py
+++ app/dependencies/repositories.py
@@ -7,7 +7,7 @@ from repositories.order import OrderRepository
 from repositories.seat import SeatRepository
 from repositories.session import SessionRepository
 from repositories.session_price import SessionPriceRepository
-from repositories.ticket import TicketRepository
+from repositories.ticket import TicketQueryRepository, TicketCommandRepository
 from repositories.unit_of_work import UnitOfWork
 from repositories.user import UserRepository
 
@@ -25,5 +25,6 @@ class RepositoryProvider(Provider):
     get_seat_repo = provide(SeatRepository)
     get_session_price_repo = provide(SessionPriceRepository)
     get_order_repo = provide(OrderRepository)
-    get_ticket_repo = provide(TicketRepository)
+    get_ticket_query_repo = provide(TicketQueryRepository)
+    get_ticket_command_repo = provide(TicketCommandRepository)
     get_user_repo = provide(UserRepository)
