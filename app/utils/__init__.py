97dcb2d079c680c1c886d430712058f2a1819010
spline <dmcemin@gmail.com>
2026-03-19 21:52:45 +0100

    delete: OrderTicketAdapter import from utils

--- app/utils/__init__.py
+++ app/utils/__init__.py
@@ -1,7 +1,6 @@
 __all__ = (
     "camel_case_to_snake_case",
     "generate_order_public_code",
-    "OrderTicketAdapter",
     "orjson_serializer",
     "orjson_deserializer"
 )
@@ -9,4 +8,3 @@ __all__ = (
 from .case_converter import camel_case_to_snake_case
 from .generate_order_public_code import generate_order_public_code
 from .orjson_serializers import orjson_serializer, orjson_deserializer
-from .order_ticket_adapter import OrderTicketAdapter
