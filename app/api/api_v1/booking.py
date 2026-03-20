daed484dafdef6a22c409fc18d88caa59358790d
spline <dmcemin@gmail.com>
2026-03-20 14:08:51 +0100

    add: get_bookings_by_user_id endpoint

--- app/api/api_v1/booking.py
+++ app/api/api_v1/booking.py
@@ -4,11 +4,24 @@ from dishka.integrations.fastapi import DishkaRoute, FromDishka
 from fastapi import APIRouter
 
 from schemas.booking import BookingOrderRead
-from usage.booking import GetBookingByNumberUsage
+from usage.booking import GetBookingByNumberUsage, GetBookingsByUserIDUsage
 
 router = APIRouter(route_class=DishkaRoute)
 
 
-@router.get("/{order_number}", summary="Get Booking")
-async def get_booking(order_number: UUID, get_booking_usage: FromDishka[GetBookingByNumberUsage]) -> BookingOrderRead:
+@router.get("/orders/{order_number}", summary="Get Booking")
+async def get_booking(
+        order_number: UUID,
+        get_booking_usage: FromDishka[GetBookingByNumberUsage]
+) -> BookingOrderRead:
     return await get_booking_usage(order_number)
+
+
+@router.get("/orders/users/{user_id}", summary="Get Bookings")
+async def get_bookings_by_user_id(
+        get_bookings_by_user_id_usage: FromDishka[GetBookingsByUserIDUsage],
+        user_id: int,
+        skip: int = 0,
+        limit: int = 100,
+) -> list[BookingOrderRead]:
+    return await get_bookings_by_user_id_usage(user_id, skip, limit)
