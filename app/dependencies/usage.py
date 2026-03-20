346f79523e144bc4e0bb388f0d7f7cc33613197c
spline <dmcemin@gmail.com>
2026-03-20 14:06:47 +0100

    add: GetBookingsByUserIDUsage provider

--- app/dependencies/usage.py
+++ app/dependencies/usage.py
@@ -1,9 +1,10 @@
 from dishka import provide, Provider, Scope
 
-from usage.booking import GetBookingByNumberUsage
+from usage.booking import GetBookingByNumberUsage, GetBookingsByUserIDUsage
 
 
 class UsageProvider(Provider):
     scope = Scope.REQUEST
 
     get_booking_by_number_usage = provide(GetBookingByNumberUsage)
+    get_bookings_by_user_id_usage = provide(GetBookingsByUserIDUsage)
