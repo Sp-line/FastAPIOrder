e32e8e7b10c4a86245b30a3f327d110fbe2a514c
spline <dmcemin@gmail.com>
2026-03-22 19:58:46 +0100

    delete: get_ticket_by_public_code in ticket endpoints

--- app/api/api_v1/ticket.py
+++ app/api/api_v1/ticket.py
@@ -1,5 +1,3 @@
-from uuid import UUID
-
 from dishka.integrations.fastapi import (
     DishkaRoute,
     FromDishka,
@@ -28,11 +26,6 @@ async def get_tickets(service: FromDishka[TicketQueryService], skip: int = 0, li
 @router.get("/{ticket_id}", summary="[Admin] Get Ticket")
 async def get_ticket(ticket_id: int, service: FromDishka[TicketQueryService]) -> TicketRead:
     return await service.get_by_id(ticket_id)
-
-
-@router.get("/public/{public_code}")
-async def get_ticket_by_public_code(public_code: UUID, service: FromDishka[TicketQueryService]) -> TicketRead:
-    return await service.get_by_public_code(public_code)
 
 
 @router.get("/list/{user_id}")
