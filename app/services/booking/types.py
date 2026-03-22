07afda21cfb95623f9e7b7d382a51f0acce30636
spline <dmcemin@gmail.com>
2026-03-22 21:36:07 +0100

    add: OrderMap type

--- app/services/booking/types.py
+++ app/services/booking/types.py
@@ -1,11 +1,17 @@
 from typing import TYPE_CHECKING
 
 if TYPE_CHECKING:
-    from core.models import Session, Seat, SessionPrice
+    from core.models import (
+        Session,
+        Seat,
+        SessionPrice,
+        Order
+    )
     from constants import SeatType
 
 type SessionPriceCombinationTuple = tuple[int, SeatType]
 
+type OrderMap = dict[int, Order]
 type SessionMap = dict[int, Session]
 type SeatMap = dict[int, Seat]
 type PriceMap = dict[SessionPriceCombinationTuple, SessionPrice]
