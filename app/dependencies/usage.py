d5560d9d98f3104db762e1b04ad16c76fa5c8aaa
spline <dmcemin@gmail.com>
2026-03-22 20:00:14 +0100

    add: GetTicketByPublicCodeUsage provider

--- app/dependencies/usage.py
+++ app/dependencies/usage.py
@@ -4,8 +4,16 @@ from dishka import (
     Scope
 )
 
-from usage.booking import GetBookingByNumberUsage, GetBookingsByUserIDUsage, CreateBookingUsage
-from usage.ticket import AddTicketToOrderUsage, RemoveTicketFromOrderUsage
+from usage.booking import (
+    GetBookingByNumberUsage,
+    GetBookingsByUserIDUsage,
+    CreateBookingUsage,
+    GetTicketByPublicCodeUsage
+)
+from usage.ticket import (
+    AddTicketToOrderUsage,
+    RemoveTicketFromOrderUsage
+)
 
 
 class UsageProvider(Provider):
@@ -14,6 +22,7 @@ class UsageProvider(Provider):
     get_booking_create_usage = provide(CreateBookingUsage)
     get_booking_by_number_usage = provide(GetBookingByNumberUsage)
     get_bookings_by_user_id_usage = provide(GetBookingsByUserIDUsage)
+    get_ticket_by_public_code_usage = provide(GetTicketByPublicCodeUsage)
 
     get_add_ticket_to_order_usage = provide(AddTicketToOrderUsage)
     get_remove_ticket_from_order_usage = provide(RemoveTicketFromOrderUsage)
