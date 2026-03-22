166d59566424ea9578515ea88c1a3538fd811ace
spline <dmcemin@gmail.com>
2026-03-22 19:58:14 +0100

    delete: get_by_public_code from TicketQueryService

--- app/services/ticket.py
+++ app/services/ticket.py
@@ -1,7 +1,4 @@
-from uuid import UUID
-
 from core.models import Ticket
-from exceptions.db import ObjectNotFoundException
 from repositories.ticket import TicketQueryRepository
 from schemas.ticket import TicketRead
 from services import (
@@ -22,16 +19,6 @@ class TicketQueryService(
             table_name="tickets",
             read_schema=TicketRead,
         )
-
-    async def get_by_public_code(self, public_code: UUID) -> TicketRead:
-        if not (obj := await self._repository.get_by_public_code(public_code)):
-            raise ObjectNotFoundException(
-                conditions={
-                    "public_code": public_code,
-                },
-                table_name=self._table_name
-            )
-        return self._read_schema.model_validate(obj)
 
     async def get_by_user_id(self, user_id: int, skip: int = 0, limit: int = 100) -> list[TicketRead]:
         objs = await self._repository.get_by_user_id(user_id=user_id, skip=skip, limit=limit)
