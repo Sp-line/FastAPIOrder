47f4f6b23649ed002cc6e4bb654fa862d4561256
spline <dmcemin@gmail.com>
2026-03-22 14:58:37 +0100

    add: DataExistenceService dependencies

--- app/dependencies/services.py
+++ app/dependencies/services.py
@@ -1,14 +1,14 @@
 from dishka import provide, Scope, Provider, alias
 
-from domain.booking import BookingDomain
 from services.booking import BookingDataAssembler, TicketBuilderService, DefaultPricing, PricingStrategy, \
     OrderSchedulerService
 from services.hall import HallService
 from services.movie import MovieService
-from services.seat import SeatService
-from services.session import SessionService
-from services.session_price import SessionPriceService
-from services.ticket import TicketQueryService
+from services.order import OrderQueryService, OrderDataExistenceService
+from services.seat import SeatService, SeatDataExistenceService
+from services.session import SessionService, SessionDataExistenceService
+from services.session_price import SessionPriceService, SessionPriceDataExistenceService
+from services.ticket import TicketQueryService, TicketDataExistenceService
 from services.user import UserService
 
 
@@ -18,17 +18,24 @@ class ServiceProvider(Provider):
     get_hall_service = provide(HallService)
 
     get_session_service = provide(SessionService)
+    get_session_data_existence = provide(SessionDataExistenceService)
 
     get_movie_service = provide(MovieService)
 
     get_seat_service = provide(SeatService)
+    get_seat_data_existence = provide(SeatDataExistenceService)
 
     get_session_price_service = provide(SessionPriceService)
+    get_session_price_data_existence = provide(SessionPriceDataExistenceService)
 
     get_user_service = provide(UserService)
-    get_ticket_query_service = provide(TicketQueryService)
 
-    get_booking_domain_service = provide(BookingDomain)
+    get_order_service = provide(OrderQueryService)
+    get_order_data_existence = provide(OrderDataExistenceService)
+
+    get_ticket_query_service = provide(TicketQueryService)
+    get_ticket_data_existence = provide(TicketDataExistenceService)
+
     get_booking_data_assembler_service = provide(BookingDataAssembler)
     get_ticket_builder_service = provide(TicketBuilderService)
     get_default_pricing = provide(DefaultPricing)
