c1654b1d889589c844829741108f38c6b91c570e
spline <dmcemin@gmail.com>
2026-03-22 15:03:54 +0100

    add: order endpoints

---
+++ app/api/api_v1/order.py
@@ -0,0 +1,17 @@
+from dishka.integrations.fastapi import DishkaRoute, FromDishka
+from fastapi import APIRouter
+
+from schemas.order import OrderAdminRead
+from services.order import OrderQueryService
+
+router = APIRouter(route_class=DishkaRoute)
+
+
+@router.get("/", summary="[Admin] Get Orders")
+async def get_orders(service: FromDishka[OrderQueryService], skip: int = 0, limit: int = 100) -> list[OrderAdminRead]:
+    return await service.get_all(skip, limit)
+
+
+@router.get("/{order_id}", summary="[Admin] Get Order")
+async def get_order(order_id: int, service: FromDishka[OrderQueryService]) -> OrderAdminRead:
+    return await service.get_by_id(order_id)
