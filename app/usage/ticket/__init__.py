1252e1245510aefd4f2339774a56c623a5d211b9
spline <dmcemin@gmail.com>
2026-03-23 15:40:25 +0100

    add: UpdateTicketStatusInOrderUsage import

--- app/usage/ticket/__init__.py
+++ app/usage/ticket/__init__.py
@@ -2,8 +2,10 @@ __all__ = (
     "AddTicketToOrderUsage",
     "RemoveTicketFromOrderUsage",
     "AddTicketsToOrdersUsage",
+    "UpdateTicketStatusInOrderUsage"
 )
 
 from usage.ticket.add_ticket_to_order import AddTicketToOrderUsage
 from usage.ticket.add_tickets_to_orders import AddTicketsToOrdersUsage
 from usage.ticket.remove_ticket_from_order import RemoveTicketFromOrderUsage
+from usage.ticket.update_ticket_status_in_order import UpdateTicketStatusInOrderUsage
