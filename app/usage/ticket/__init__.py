5545f0838b0e9029ce16434985e568ee1cac75c4
spline <dmcemin@gmail.com>
2026-03-23 22:54:29 +0100

    add: import for UpdateTicketPriceInOrderUsage

--- app/usage/ticket/__init__.py
+++ app/usage/ticket/__init__.py
@@ -2,10 +2,12 @@ __all__ = (
     "AddTicketToOrderUsage",
     "RemoveTicketFromOrderUsage",
     "AddTicketsToOrdersUsage",
-    "UpdateTicketStatusInOrderUsage"
+    "UpdateTicketStatusInOrderUsage",
+    "UpdateTicketPriceInOrderUsage",
 )
 
 from usage.ticket.add_ticket_to_order import AddTicketToOrderUsage
 from usage.ticket.add_tickets_to_orders import AddTicketsToOrdersUsage
 from usage.ticket.remove_ticket_from_order import RemoveTicketFromOrderUsage
+from usage.ticket.update_ticket_price_in_order import UpdateTicketPriceInOrderUsage
 from usage.ticket.update_ticket_status_in_order import UpdateTicketStatusInOrderUsage
