7f9e4578564a2459ed4a6ada051d91ce31371ba4
spline <dmcemin@gmail.com>
2026-03-24 19:44:56 +0100

    add: OrderDeleteUsage

---
+++ app/usage/order/delete_order.py
@@ -0,0 +1,53 @@
+from dataclasses import dataclass
+
+from constants import OrderStatus
+from domain import EnsureOrderIsSafeToDelete
+from repositories import OrderRepository, UnitOfWork
+from services import OrderDataExistenceService
+from services.booking import OrderSchedulerService
+
+
+@dataclass(frozen=True, slots=True)
+class DeleteOrderDataExistenceServices:
+    order: OrderDataExistenceService
+
+
+@dataclass(frozen=True, slots=True)
+class DeleteOrderDomain:
+    order_is_safe_to_delete: EnsureOrderIsSafeToDelete
+
+
+class OrderDeleteUsage:
+    def __init__(
+            self,
+            order_repo: OrderRepository,
+            unit_of_work: UnitOfWork,
+
+            scheduler: OrderSchedulerService,
+            domain: DeleteOrderDomain,
+            data_existence: DeleteOrderDataExistenceServices,
+    ) -> None:
+        self._order_repo = order_repo
+        self._uow = unit_of_work
+
+        self._scheduler = scheduler
+        self._domain = domain
+        self._data_existence = data_existence
+
+    async def __call__(self, order_id: int) -> None:
+        order = self._data_existence.order.ensure_obj_exist(
+            obj_id=order_id,
+            obj=await self._order_repo.get_by_id(order_id)
+        )
+        self._domain.order_is_safe_to_delete(order_status=order.status)
+
+        async with self._uow:
+            await self._order_repo.delete(obj_id=order.id)
+
+            conditions = (
+                    order.status == OrderStatus.PENDING,
+                    order.expire_schedule_id
+            )
+
+            if all(conditions):
+                await self._scheduler.cancel_task(schedule_id=order.expire_schedule_id)  # type: ignore[arg-type]
