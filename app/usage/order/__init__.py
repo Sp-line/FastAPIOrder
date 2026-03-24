584b26f94a6547144e8bec6034bc85d34ba7f1aa
spline <dmcemin@gmail.com>
2026-03-24 19:46:36 +0100

    add: OrderDeleteUsage

--- app/usage/order/__init__.py
+++ app/usage/order/__init__.py
@@ -1,9 +1,11 @@
 __all__ = (
     "OrderCreateUsage",
     "BulkCreateOrderUsage",
-    "UpdateOrderStatusUsage"
+    "UpdateOrderStatusUsage",
+    "OrderDeleteUsage",
 )
 
 from usage.order.bulk_create import BulkCreateOrderUsage
 from usage.order.create import OrderCreateUsage
+from usage.order.delete_order import OrderDeleteUsage
 from usage.order.update_status import UpdateOrderStatusUsage
