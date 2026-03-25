166dcbdb533e3ea969752126e3535a2b2e1bd87b
spline <dmcemin@gmail.com>
2026-03-25 22:34:39 +0100

    update: UpdateTicketStatusInOrderUsage

--- app/usage/ticket/update_ticket_status_in_order.py
+++ app/usage/ticket/update_ticket_status_in_order.py
@@ -1,12 +1,16 @@
+from constants import OrderStatus
 from repositories import (
     TicketRepository,
-    UnitOfWork
+    UnitOfWork,
+    OrderRepository
 )
+from schemas.order import OrderUpdateDB
 from schemas.ticket import (
     TicketStatusUpdateReq,
     TicketRead,
     TicketUpdateDB
 )
+from services import TaskScheduler
 from usage.ticket.facades import (
     UpdateTicketStatusInOrderDomain,
     UpdateTicketStatusInOrderDataExistenceServices
@@ -17,15 +21,19 @@ class UpdateTicketStatusInOrderUsage:
     def __init__(
             self,
             ticket_repo: TicketRepository,
+            order_repo: OrderRepository,
             unit_of_work: UnitOfWork,
 
+            scheduler: TaskScheduler,
             domain: UpdateTicketStatusInOrderDomain,
             data_existence: UpdateTicketStatusInOrderDataExistenceServices
     ) -> None:
         self._ticket_repo = ticket_repo
+        self._order_repo = order_repo
         self._uow = unit_of_work
 
         self._domain = domain
+        self._scheduler = scheduler
         self._data_existence = data_existence
 
     async def __call__(self, ticket_id: int, data: TicketStatusUpdateReq) -> TicketRead:
@@ -40,9 +48,42 @@ class UpdateTicketStatusInOrderUsage:
         )
 
         async with self._uow:
-            updated_ticket = await self._ticket_repo.update(
+            updated_ticket_db = await self._ticket_repo.update(
                 obj_id=ticket.id,
                 data=TicketUpdateDB(status=data.status)
             )
+            updated_ticket = self._data_existence.ticket.ensure_obj_exist(
+                obj_id=ticket_id,
+                obj=updated_ticket_db
+            )
+
+            not_updated_order = self._data_existence.order.ensure_obj_exist(
+                obj_id=updated_ticket.order_id,
+                obj=await self._order_repo.get_with_tickets(updated_ticket.order_id)
+            )
+
+            if not_updated_order.tickets:
+                ticket_statuses = {ticket.status for ticket in not_updated_order.tickets}
+
+                new_order_status = self._domain.sync_order_status_with_tickets(
+                    order_status=not_updated_order.status,
+                    ticket_statuses=ticket_statuses
+                )
+
+                if new_order_status:
+                    await self._order_repo.update(
+                        obj_id=not_updated_order.id,
+                        data=OrderUpdateDB(status=new_order_status)
+                    )
+
+                    conditions = (
+                        not_updated_order.status == OrderStatus.PENDING,
+                        not_updated_order.expire_schedule_id
+                    )
+
+                    if all(conditions):
+                        await self._scheduler.cancel_schedule(
+                            schedule_id=not_updated_order.expire_schedule_id  # type: ignore[arg-type]
+                        )
 
             return TicketRead.model_validate(updated_ticket)
