a545704bb700a212c8c1ef60f530cb0094139b3f
spline <dmcemin@gmail.com>
2026-03-25 22:33:52 +0100

    change: UpdateTicketStatus facades

--- app/usage/ticket/facades/update_ticket_status_in_order.py
+++ app/usage/ticket/facades/update_ticket_status_in_order.py
@@ -1,14 +1,16 @@
 from dataclasses import dataclass
 
-from domain import EnsureValidTicketStatusTransition
-from services import TicketDataExistenceService
+from domain import EnsureValidTicketStatusTransition, SyncOrderStatusWithTickets
+from services import TicketDataExistenceService, OrderDataExistenceService
 
 
 @dataclass(frozen=True, slots=True)
 class UpdateTicketStatusInOrderDataExistenceServices:
     ticket: TicketDataExistenceService
+    order: OrderDataExistenceService
 
 
 @dataclass(frozen=True, slots=True)
 class UpdateTicketStatusInOrderDomain:
     valid_ticket_status_transition: EnsureValidTicketStatusTransition
+    sync_order_status_with_tickets: SyncOrderStatusWithTickets
