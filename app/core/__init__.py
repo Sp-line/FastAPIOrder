9678cdf29c56d7af8dedff6b5f16e24a0e5f0f4d
spline <dmcemin@gmail.com>
2026-03-27 14:58:22 +0100

    add: router, catalog_stream, showtimes_stream imports

--- app/core/__init__.py
+++ app/core/__init__.py
@@ -1,8 +1,16 @@
 __all__ = (
     "broker",
     "redis_source",
+    "fs_router",
+    "catalog_stream",
+    "showtimes_stream"
 )
 
+from .fs_router import (
+    router as fs_router,
+    catalog_stream,
+    showtimes_stream
+)
 from .taskiq_broker import (
     broker,
     redis_source,
