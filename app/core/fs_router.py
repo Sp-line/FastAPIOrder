eb9a6bbba77c4f3d9e2f7436687f661a2f770b46
spline <dmcemin@gmail.com>
2026-03-27 14:57:36 +0100

    add: router, catalog_stream, showtimes_stream

---
+++ app/core/fs_router.py
@@ -0,0 +1,22 @@
+import logging
+
+from fastapi import FastAPI
+from faststream.nats import JStream
+from faststream.nats.fastapi import NatsRouter
+
+from core.config import settings
+
+router = NatsRouter(str(settings.faststream.nats_url))
+
+catalog_stream = JStream(name="catalog_stream", declare=False)
+
+showtimes_stream = JStream("showtimes_stream", declare=False)
+
+
+@router.after_startup
+async def configure_logging(app: FastAPI) -> None:
+    logging.basicConfig(
+        level=settings.logging.log_level_value,
+        format=settings.logging.log_format,
+        datefmt=settings.logging.log_datefmt,
+    )
