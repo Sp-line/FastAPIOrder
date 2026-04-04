03dfe6dce378f0b2a2973c01112f8e108dd21a9e
spline <dmcemin@gmail.com>
2026-04-04 15:14:26 +0200

    delete: CRUDEventPublishers

--- app/events/order.py
+++ app/events/order.py
@@ -1,6 +1,5 @@
 from core import fs_router
 from schemas.base import Id
-from schemas.events import CRUDEventPublishers
 from schemas.order import (
     OrderCreateEvent,
     OrderUpdateEvent
@@ -29,12 +28,4 @@ order_bulk_updated = fs_router.publisher(
 order_deleted = fs_router.publisher(
     "purchases.orders.deleted",
     schema=Id,
-)
-
-order_crud_publishers = CRUDEventPublishers(
-    create_pub=order_created,
-    bulk_create_pub=order_bulk_created,
-    update_pub=order_updated,
-    bulk_update_pub=order_bulk_updated,
-    delete_pub=order_deleted,
 )
--- app/events/ticket.py
+++ app/events/ticket.py
@@ -1,6 +1,5 @@
 from core import fs_router
 from schemas.base import Id
-from schemas.events import CRUDEventPublishers
 from schemas.ticket import (
     TicketCreateEvent,
     TicketUpdateEvent
@@ -29,12 +28,4 @@ ticket_bulk_updated = fs_router.publisher(
 ticket_deleted = fs_router.publisher(
     "purchases.tickets.deleted",
     schema=Id,
-)
-
-ticket_crud_publishers = CRUDEventPublishers(
-    create_pub=ticket_created,
-    bulk_create_pub=ticket_bulk_created,
-    update_pub=ticket_updated,
-    bulk_update_pub=ticket_bulk_updated,
-    delete_pub=ticket_deleted,
 )
