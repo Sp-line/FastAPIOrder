83037f78cfb685b5cfa2289c4e2140e81d396b27
spline <dmcemin@gmail.com>
2026-04-04 19:56:26 +0200

    delete: NatsMsgIdDep event type import

--- app/app_types/__init__.py
+++ app/app_types/__init__.py
@@ -2,11 +2,9 @@ __all__ = (
     "IntMap",
     "PriceMap",
     "TicketPricingData",
-    "NatsMsgIdDep"
 )
 
 from .base import IntMap
-from .events import NatsMsgIdDep
 from .session_price import (
     PriceMap,
     TicketPricingData
