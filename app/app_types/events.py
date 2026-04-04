88b2fdc13ee47264e0b0da22766882cfc749bf18
spline <dmcemin@gmail.com>
2026-04-04 18:20:17 +0200

    delete: event types

--- app/app_types/events.py
+++
@@ -1,7 +0,0 @@
-from typing import TypeAlias, Annotated
-from uuid import UUID
-
-from faststream.nats.fastapi import Context
-
-# noinspection PyUnresolvedReferences
-NatsMsgIdDep: TypeAlias = Annotated[UUID | None, Context("message.headers.Nats-Msg-Id", default=None)]
